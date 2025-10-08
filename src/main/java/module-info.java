0eb1e0f4c32f3a07ad66576166e08786e207b13a
oCaduu_ <ocarlosdigital09@gmail.com>
2025-10-08 14:11:03 -0300

    Finalizando a estruturação do projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module org.syscimc {
 
     opens org.syscimc to javafx.fxml;
     exports org.syscimc;
+    exports org.syscimc.controler;
+    opens org.syscimc.controler to javafx.fxml;
 }
--- src/main/java/org/syscimc/controler/HelloController.java
+++ src/main/java/org/syscimc/controler/HelloController.java
@@ -1,4 +1,4 @@
-package org.syscimc;
+package org.syscimc.controler;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
